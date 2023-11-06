a859a55e23c267e8bc7d2c5e9fb21fded68c0b3e
Fabio de Moura Martins <fabio.mmouras@hotmail.com>
2023-11-06 11:32:13 -0300

    feat: creating header

---
+++ capputeeno/src/components/Header.tsx
@@ -0,0 +1,44 @@
+'use client';
+
+import styled from 'styled-components';
+import { Saira_Stencil_One } from 'next/font/google';
+import { PrimaryInput, PrimaryInputWSearchIcon } from './primary-input';
+import { CartControl } from './cart-control';
+
+const sairaStence = Saira_Stencil_One({
+    weight: ['400'],
+    subsets: ['latin']
+});
+
+const TagHeader = styled.header`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 20px 160px;
+
+    >div{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        gap: 24px;
+    }
+`;
+
+const Logo = styled.a`
+    color: var(--logo-color);
+    font-size: 40px;
+    font-style: normal;
+    font-weight: 400;
+`;
+
+export function Header() {
+    return (
+        <TagHeader>
+            <Logo className={sairaStence.className}>capputeeno</Logo>
+            <div>
+                <PrimaryInputWSearchIcon placeholder='Procurando por algo específico?' />
+                <CartControl/>
+            </div>
+        </TagHeader>
+    );
+}
