a241a5ed2e801d5540c49b5098f3ce37fd8242aa
Fabio de Moura Martins <fabio.mmouras@hotmail.com>
2023-11-09 09:32:11 -0300

    feat: created filter bar

---
+++ capputeeno/src/components/filter-by-priority.tsx
@@ -0,0 +1,105 @@
+import styled from 'styled-components';
+import { ArrowIcon } from './arrow-icon';
+import { useState } from 'react';
+import { useFilter } from '@/hooks/useFilter';
+import { PriorityTypes } from '@/types/priority-types';
+
+const FilterContainer = styled.div`
+    display: flex;
+    align-items: center;
+    position: relative;
+
+    button {
+        font-family: inherit;
+        font-size: 14px;
+        cursor: pointer;
+        font-style: normal;
+        font-weight: 400;
+        line-height: 22px;
+        color: var(--text-dark);
+        border: none;
+        background: transparent;
+
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        gap: 16px;
+    }
+`;
+
+const PriorityFilter = styled.ul`
+    position: absolute;
+    width: 176px;
+    border-radius: 4px;
+    background: #fff;
+    box-shadow: 0px 4px 12px 0px rgba(0, 0, 0, 0.1);
+    padding: 12px 16px;
+    list-style: none;
+
+    top: 100%;
+
+    li {
+        font-size: 14px;
+        line-height: 22px;
+        color: var(--text-dark);
+        cursor: pointer;
+    }
+
+    li + li {
+        margin-top: 4px;
+    }
+`;
+
+export function FilterByPriority() {
+    const [isOpen, setIsOpen] = useState(false);
+    const { setPriority } = useFilter();
+
+    const handleOpen = () => {
+        setIsOpen(prev => !prev);
+    };
+
+    const handleUpdateByPriority = (value: PriorityTypes) => {
+        setPriority(value);
+        setIsOpen(false)
+    };
+    return (
+        <FilterContainer>
+            <button onClick={handleOpen}>
+                Organizar por
+                <ArrowIcon />
+            </button>
+            {isOpen && (
+                <PriorityFilter>
+                    <li
+                        onClick={() =>
+                            handleUpdateByPriority(PriorityTypes.NEWS)
+                        }
+                    >
+                        Novidades
+                    </li>
+                    <li
+                        onClick={() =>
+                            handleUpdateByPriority(PriorityTypes.BIGGEST_PRICE)
+                        }
+                    >
+                        Preço: Maior - menor
+                    </li>
+                    <li
+                        onClick={() =>
+                            handleUpdateByPriority(PriorityTypes.MINOR_PRICE)
+                        }
+                    >
+                        Preço: Menor - maior
+                    </li>
+                    <li
+                        onClick={() =>
+                            handleUpdateByPriority(PriorityTypes.POPULARITY)
+                        }
+                    >
+                        Mais Vendidos
+                    </li>
+                </PriorityFilter>
+            )}
+        </FilterContainer>
+    );
+}
---
+++ capputeeno/src/components/filter-by-type.tsx
@@ -0,0 +1,61 @@
+import { useFilter } from '@/hooks/useFilter';
+import { FilterType } from '@/types/filter-types';
+import styled from 'styled-components';
+
+interface FilterItemProps {
+    selected: boolean;
+}
+
+interface FilterByTypeProps {}
+
+const FilterList = styled.ul`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 40px;
+    list-style: none;
+`;
+
+const FilterItem = styled.li<FilterItemProps>`
+    color: var(--text-dark);
+    font-size: 16px;
+    font-family: inherit;
+    font-weight: ${props => (props.selected ? '600' : '400')};
+    line-height: 22px; /* 137.5% */
+    text-transform: uppercase;
+    cursor: pointer;
+
+    border-bottom: ${props =>
+        props.selected ? '4px solid var(--orange-low)' : ''};
+`;
+
+export function FilterByType() {
+    const { type, setType } = useFilter();
+
+    const handleChangeByType = (value: FilterType) => {
+        setType(value);
+    };
+
+    return (
+        <FilterList>
+            <FilterItem
+                selected={type === FilterType.ALL}
+                onClick={() => handleChangeByType(FilterType.ALL)}
+            >
+                Todos os produtos
+            </FilterItem>
+            <FilterItem
+                selected={type === FilterType.SHIRT}
+                onClick={() => handleChangeByType(FilterType.SHIRT)}
+            >
+                Camisetas
+            </FilterItem>
+            <FilterItem
+                selected={type === FilterType.MUG}
+                onClick={() => handleChangeByType(FilterType.MUG)}
+            >
+                Canecas
+            </FilterItem>
+        </FilterList>
+    );
+}
