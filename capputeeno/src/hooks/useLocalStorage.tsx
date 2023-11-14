3722d53c2d2e34525716151dda38071c5c963061
Fabio de Moura Martins <fabio.mmouras@hotmail.com>
2023-11-14 15:33:04 -0300

    fix : localstorage config

--- capputeeno/src/hooks/useLocalStorage.tsx
+++ capputeeno/src/hooks/useLocalStorage.tsx
@@ -4,9 +4,12 @@ export function useLocalStorage<T>(item: string, initialValue: T) {
     const [value, setValue] = useState<T>(initialValue);
 
     useEffect(() => {
-        // if (typeof window === 'undefined') return;
-
-        setValue(JSON.parse(localStorage.getItem(item) ?? ''));
+        if (typeof window === 'undefined') return;
+        let value = localStorage.getItem(item);
+        if(value){
+            setValue(JSON.parse(value));
+        }
+        
     }, [item]);
 
     const updateLocalStorage = (newValue: T) => {
