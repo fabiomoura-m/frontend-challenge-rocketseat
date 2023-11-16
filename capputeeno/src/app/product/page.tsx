f13ee89a3d20e9db10a53a93ee7730cc6ca40d37
Fabio de Moura Martins <fabio.mmouras@hotmail.com>
2023-11-16 13:35:17 -0300

    feat: add btn add to cart control

--- capputeeno/src/app/product/page.tsx
+++ capputeeno/src/app/product/page.tsx
@@ -49,8 +49,6 @@ const Container = styled.div`
                 align-items: center;
                 gap: 12px;
             }
-
-
         }
     }
 `;
@@ -117,6 +115,35 @@ export default function Products({
 }) {
     const { data } = useProduct(searchParams.id);
 
+    const handleAddToCart = () => {
+        let cartItems = localStorage.getItem('cart-items');
+
+        if (cartItems) {
+            let cartItemsArray = JSON.parse(cartItems);
+
+            let existingProductIndex = cartItemsArray.findIndex(
+                (item: { id: string }) => item.id === searchParams.id
+            );
+
+            if (existingProductIndex != -1) {
+                cartItemsArray[existingProductIndex].quantity += 1;
+            } else {
+                cartItemsArray.push({ ...data, id: searchParams.id, quantity: 1 });
+            }
+
+            localStorage.setItem('cart-items', JSON.stringify(cartItemsArray));
+        } else {
+            const newCart = [
+                {
+                    ...data,
+                    id: searchParams.id,
+                    quantity: 1
+                }
+            ];
+            localStorage.setItem('cart-items', JSON.stringify(newCart));
+        }
+    };
+
     return (
         <DefaultPageLayout>
             <Container>
@@ -144,7 +171,7 @@ export default function Products({
                                 <p>{data?.description}</p>
                             </div>
                         </ProductInfo>
-                        <button>
+                        <button onClick={handleAddToCart}>
                             <ShopBagIcon />
                             Adicionar ao carrinho
                         </button>
