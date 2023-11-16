f567509f54042e6fb60d834024feb49c560e6050
Fabio de Moura Martins <fabio.mmouras@hotmail.com>
2023-11-16 10:54:12 -0300

    feat: add navigation to product page

--- capputeeno/src/components/product-card.tsx
+++ capputeeno/src/components/product-card.tsx
@@ -1,11 +1,13 @@
 import { formatPrice } from '@/utils/formatPrice';
 import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 import { styled } from 'styled-components';
 
 interface ProductCardProps {
     image: string;
     title: string;
     price: number;
+    id: string;
 }
 
 const Card = styled.div`
@@ -13,6 +15,7 @@ const Card = styled.div`
     align-items: center;
     justify-content: center;
     flex-direction: column;
+    cursor: pointer;
 
     border-radius: 0 0 4px 4px;
     background: rgba(255, 255, 255, 0.4);
@@ -55,9 +58,14 @@ const Card = styled.div`
 
 export function ProductCard(props: ProductCardProps) { 
     const price = formatPrice(props.price)
+    const router = useRouter()
+
+    const handleNavigate = () =>{
+        router.push(`/product?id=${props.id}`)
+    }
 
     return (
-        <Card>
+        <Card onClick={handleNavigate}>
             <Image src={props.image} alt="" width={256} height={232} />
             <div>
                 <h3>{props.title}</h3>
